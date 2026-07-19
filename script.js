7e74da176ef1c8661c67cb3421bbe1a50eade991
grace haddad <gracehadda@gmail.com>
2026-07-19 21:20:54 +0300

    save data to the browser

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function addTask()
 		li.appendChild(span);
 	}
 	inputBox.value = "";
+	saveData();
 }
 
 listContainer.addEventListener("click", function(e)
@@ -24,10 +25,22 @@ listContainer.addEventListener("click", function(e)
 	if (e.target.tagName === "LI")
 	{
 		e.target.classList.toggle("checked");
+		saveData();
 	}
 	else if (e.target.tagName === "SPAN")
 	{
 		e.target.parentElement.remove();
+		saveData();
 	}
 }, false);
 
+function saveData()
+{
+	localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+	listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
